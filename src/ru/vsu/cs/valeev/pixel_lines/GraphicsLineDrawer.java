aa4fdcbc0c201dcf4ad1c54b2246b52509cbc245
Victor Valeev <viktorval2001@gmail.com>
2020-10-16 22:30:24 +0300

    a little change

--- src/ru/vsu/cs/valeev/pixel_lines/GraphicsLineDrawer.java
+++ src/ru/vsu/cs/valeev/pixel_lines/GraphicsLineDrawer.java
@@ -9,6 +9,11 @@ public class GraphicsLineDrawer implements LineDrawer {
         this.g = g;
     }
 
+    @Override
+    public void drawLine(int x1, int y1, int x2, int y2, Color color) {
+
+    }
+
     public void drawLine(int x1, int y1, int x2, int y2) {
         g.drawLine(x1, y1, x2, y2);
     }
